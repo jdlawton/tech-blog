42c9d01d469058e5ebf640983c03b1061a2d4ba7
JoeL <jlawton1203@gmail.com>
2020-08-27 15:19:15 -0500

    added post routes

--- models/index.js
+++ models/index.js
@@ -3,6 +3,13 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 //associations go here
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 
 module.exports = {User, Post, Comment};
